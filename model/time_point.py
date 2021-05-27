1b4ff4c9dc9330b6703cc44806d695aa63e6bb8b
chally <chally-portable@DESKTOP-BUOJ68C>
2021-05-27 02:30:52 -0700

    Added cosntructor via string

--- model/time_point.py
+++ model/time_point.py
@@ -1,11 +1,22 @@
 
 class TimePoint :
+
     def __init__(self, param) :
-        self._milliseconds = param * 1000
+        self._milliseconds = TimePoint.MATCH[type(param)](param)
 
     @staticmethod
     def from_milliseconds(value) :
         return TimePoint(value / 1000.0)
+
+    def time_from_string(x) :
+        mins = int(x[:x.find(':')])
+        secs = int(x[x.find(':') + 1:])
+        return 1000 * (mins * 60 + secs)
+
+    MATCH = {
+        int: lambda x: x * 1000,
+        str: time_from_string
+    }
 
     def _get_time(self) :
         return self._milliseconds
