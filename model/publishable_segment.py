ede92556c06988436cfcb79e4de6966b3589b691
Chalinda Rodrigo <nobleservant49@gmail.com>
2021-09-07 18:54:10 +1000

    Fixed typing

--- model/publishable_segment.py
+++ model/publishable_segment.py
@@ -1,5 +1,7 @@
+from pydub import AudioSegment
+
 class PublishableSegment :
-    def __init__(self, segment: AudioSegment, tags: Dictionary) :
+    def __init__(self, segment: AudioSegment, tags: dict) :
 
         self._segment = segment
         self._tags = tags 
